e4d78af7ac1fa05b9cab5c9182667a68192bdc9c
unknown <arkadiuszhorwat@gmail.com>
2021-04-20 14:46:14 +0200

    make connection with database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
-app.listen(3001, () => {
-    console.log('running on port 3001');
+app.use(express.json());
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todolist'
+});
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(3002, () => {
+    console.log('running on port 3002');
 });
